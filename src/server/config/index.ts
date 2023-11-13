5f0bd75a7133a64b10913b97e7c28a16c5f41d6a
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-11-13 17:56:52 -0600

    adding a hasUndefinedValues validator for more steamlined and reusable error handling

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
+import validators from "../utils/validators";
 
 dotenv.config();
+
+const { hasUndefinedValues } = validators;
 
 const pg = {
     user: process.env.DB_USER as string,
@@ -9,14 +12,14 @@ const pg = {
     database: process.env.DB_SCHEMA as string,
 };
 
-if (!pg.user || !pg.password || !pg.host || !pg.database) throw new Error("Missing Postgres Envars");
+if (hasUndefinedValues(pg)) throw new Error("Missing Postgres Envars");
 
 const jwt = {
     secret: process.env.JWT_KEY as string,
     expiration: process.env.JWT_EXPIRATION as string,
 };
 
-if (!jwt.secret || !jwt.expiration) throw new Error("Missing JWT Envars");
+if (hasUndefinedValues(jwt)) throw new Error("Missing JWT Envars");
 
 //! add mailgun env
 const mailgun = {
@@ -25,17 +28,28 @@ const mailgun = {
     key: process.env.MAILGUN_API_DEY as string,
 };
 
-if (!mailgun.to || !mailgun.domain || !mailgun.key) throw new Error("Missing Mailgun Envars");
+if (hasUndefinedValues(mailgun)) throw new Error("Missing Mailgun Envars");
 
 const domain = {
     base: process.env.DOMAIN_BASE as string,
 };
 
-if (!domain.base) throw new Error("Missing Domain Envars");
+if (hasUndefinedValues(domain)) throw new Error("Missing Domain Envars");
+
+// Get a twilio account up and running
+const twilio = {
+    gvoice: process.env.GVOICE_NUM,
+    number: process.env.TWILIO_NUM,
+    sid: process.env.TWILIO_SID,
+    token: process.env.TWILIO_TOKEN,
+};
+
+if (hasUndefinedValues(twilio)) throw new Error("Missing Twilio Envars");
 
 export default {
     pg,
     jwt,
     mailgun,
     domain,
+    twilio,
 };
--- src/server/utils/validators.ts
+++ src/server/utils/validators.ts
@@ -1,4 +1,4 @@
-const isDefined = (value: number | boolean) => value !== undefined;
+const isDefined = (value: string | number | boolean) => value !== undefined;
 const isString = (value: string) => typeof value === "string" && value;
 const isNumber = (value: number) => typeof value === "number";
 const isBoolean = (value: boolean) => typeof value === "boolean";
@@ -11,10 +11,15 @@ const stringsAreGood = (values: string[]) => {
     };
 };
 
+const hasUndefinedValues = (obj: { [key: string]: unknown }) => {
+    return Object.values(obj).some((val) => val === undefined);
+};
+
 export default {
     isDefined,
     isString,
     isNumber,
     isBoolean,
     stringsAreGood,
+    hasUndefinedValues,
 };
